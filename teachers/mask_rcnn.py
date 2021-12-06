52cb648988371f4c55ada0c26b56e453fdbbe2d0
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-06 01:42:25 -0500

    add idx

--- teachers/mask_rcnn.py
+++ teachers/mask_rcnn.py
@@ -1,1 +1,36 @@
-# Copy paste mask rcnn implementation here
+import torch
+import torchvision
+
+MODEL = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+
+def images_to_masks(images):
+    """
+    Given a list of images, returns a list of masks.
+    """
+    results = MODEL(images)
+
+    image_segs = []
+    for image, result in zip(images, results):
+        image_seg = torch.zeros((5, *image.shape))
+
+        n_boxes = len(result['boxes'])
+        labels_numpy = result['labels'].detach().numpy()
+
+        masks_numpy = result['masks'].detach().numpy() > 0.1
+        for i in range(n_boxes-1, -1, -1):
+            if labels_numpy[i] < 5:
+                image_seg[labels_numpy[i]][masks_numpy[i][0]] += masks_numpy
+        image_segs.append(image_seg)
+    return torch.stack(image_segs, dim=0)
+
+
+def generate_and_store_masks(root_dir):
+    """
+    Generates and stores all masks
+    """
+    
+    
+
+
+
