df833c49a01bbf215d902a8eef538363ec03d75c
William Zhao <williamzhao@dhcp-10-29-120-250.dyn.mit.edu>
2021-12-06 03:20:36 -0500

    bug fix

--- teachers/run_masks.py
+++ teachers/run_masks.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-sys.path.append(Path(__file__).resolve().parents[2])
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 import numpy as np
 from pathlib import Path
